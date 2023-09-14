7adce627cb97d9d14dbe6dd8a520f5ceeb286577
Raiane <raiane.oliveira404@gmail.com>
2023-09-14 15:42:35 -0300

    fix: progress bar logic and amount slides

--- src/pages/home/components/Services.tsx
+++ src/pages/home/components/Services.tsx
@@ -41,10 +41,10 @@ export function Services() {
 
   const endSlide =
     isSliderLoaded && instanceRef.current
-      ? instanceRef.current.track.details.slides.length - 1
+      ? instanceRef.current.track.details.slides.length
       : 0
 
-  const rateSlide = Math.min((currentSlide + 1) / endSlide, 1)
+  const rateSlide = Math.ceil(100 / endSlide)
 
   return (
     <div className="bg-secondary pt-28 pb-14 pl-6" id="services">
@@ -72,7 +72,7 @@ export function Services() {
           <BoxServices
             title="Hidratação Facial"
             category="Rosto"
-            style={{ minWidth: 558, width: '100%' }}
+            style={{ minWidth: 558, maxWidth: 558, width: '100%' }}
           >
             <Image
               quality={100}
@@ -87,7 +87,7 @@ export function Services() {
           <BoxServices
             title="Estética Corporal"
             category="Corpo"
-            style={{ minWidth: 362, width: '100%' }}
+            style={{ minWidth: 362, maxWidth: 362, width: '100%' }}
           >
             <Image
               quality={100}
@@ -102,7 +102,52 @@ export function Services() {
           <BoxServices
             title="Saúde da Pele"
             category="Pele"
-            style={{ minWidth: 362, width: '100%' }}
+            style={{ minWidth: 362, maxWidth: 362, width: '100%' }}
+          >
+            <Image
+              quality={100}
+              src={Woman2}
+              alt=""
+              className="h-full w-full object-cover"
+              width={362}
+              height={434}
+            />
+          </BoxServices>
+
+          <BoxServices
+            title="Hidratação Facial"
+            category="Rosto"
+            style={{ minWidth: 558, maxWidth: 558, width: '100%' }}
+          >
+            <Image
+              quality={100}
+              src={Woman1}
+              alt=""
+              className="h-full w-full object-cover"
+              width={558}
+              height={434}
+            />
+          </BoxServices>
+
+          <BoxServices
+            title="Estética Corporal"
+            category="Corpo"
+            style={{ minWidth: 362, maxWidth: 362, width: '100%' }}
+          >
+            <Image
+              quality={100}
+              src={WomanPerfil}
+              alt=""
+              width={362}
+              height={434}
+              className="h-full w-full object-cover"
+            />
+          </BoxServices>
+
+          <BoxServices
+            title="Saúde da Pele"
+            category="Pele"
+            style={{ minWidth: 362, maxWidth: 362, width: '100%' }}
           >
             <Image
               quality={100}
@@ -117,7 +162,7 @@ export function Services() {
 
         <div className="flex items-center mt-14 gap-3 container pr-6">
           {instanceRef.current && isSliderLoaded && (
-            <Texts.Description className="text-white/50 whitespace-nowrap max-sm:text-base">
+            <Texts.Description className="text-white/50 whitespace-nowrap">
               <span className="text-white">
                 {String(currentSlide + 1).padStart(2, '0')}
               </span>{' '}
@@ -133,7 +178,7 @@ export function Services() {
             <div className="max-w-5xl w-full bg-white/30">
               <div
                 className="bg-white rounded relative h-[0.09rem] w-0 transition-[width] duration-300"
-                style={{ width: String(100 * rateSlide) + '%' }}
+                style={{ width: String(rateSlide * (currentSlide + 1)) + '%' }}
               >
                 <div
                   className="absolute rounded-full right-0 top-1/2 -translate-y-1/2 bg-white w-[0.625rem] h-[0.625rem]"
